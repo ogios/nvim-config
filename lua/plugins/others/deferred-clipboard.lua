7d3a582cc29d42908dcbe3509d5276679edf86bd
ogios <ogios@foxmail.com>
2025-04-11 11:22:29 +0800

    other stuff

--- lua/plugins/others/deferred-clipboard.lua
+++
@@ -1,9 +0,0 @@
-return {
-  "EtiamNullam/deferred-clipboard.nvim",
-  config = function()
-    require("deferred-clipboard").setup({
-      fallback = "unnamedplus",
-      lazy = true,
-    })
-  end,
-}
--- lua/plugins/others/git-conflict.lua
+++ lua/plugins/others/git-conflict.lua
@@ -1,5 +1,6 @@
 return {
   "akinsho/git-conflict.nvim",
+  event = "BufEnter",
   after = "tokyonight.nvim",
   config = function()
     require("git-conflict").setup({
