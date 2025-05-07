cbcace66a25958e858d3f414d7f9735e52e49d40
ogios <ogios@foxmail.com>
2025-05-07 17:45:30 +0800

    use avante and copilot

--- lua/plugins/others/avante.lua
+++ lua/plugins/others/avante.lua
@@ -1,7 +1,7 @@
 return {
   "yetone/avante.nvim",
   event = "VeryLazy",
-  enabled = false,
+  -- enabled = false,
   version = false, -- Never set this value to "*"! Never!
   opts = {
     provider = "copilot",
@@ -61,11 +61,50 @@ return {
     "echasnovski/mini.icons",
     {
       "zbirenbaum/copilot.lua",
-      -- cmd = "Copilot",
+      cmd = "Copilot",
+      dependencies = {
+        {
+          "nvim-lualine/lualine.nvim",
+          dependencies = {
+            { "AndreM222/copilot-lualine" },
+          },
+          opts = function(_, opts)
+            table.insert(opts.sections.lualine_x, 2, "copilot")
+          end,
+        },
+      },
+      keys = {
+        -- run `:Copilot panel` when press <M-w> under `n` or `i` mode
+        {
+          "<M-w>",
+          function()
+            require("copilot.panel").toggle()
+          end,
+          mode = { "n", "i" },
+          desc = "Copilot panel",
+        },
+      },
       -- event = "InsertEnter",
       config = function()
         require("copilot").setup({
           copilot_model = "gpt-4o-copilot",
+          panel = {
+            layout = {
+              position = "right",
+            },
+          },
+          suggestion = {
+            auto_trigger = true,
+            debounce = 200,
+            keymap = {
+              accept = "<M-Tab>",
+              accept_word = false,
+              accept_line = false,
+              next = "<M-]>",
+              prev = "<M-[>",
+              dismiss = false,
+            },
+          },
         })
       end,
     },
