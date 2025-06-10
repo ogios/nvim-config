78000912b23b9724e9b09107bee15b864235f0c2
ogios <ogios@foxmail.com>
2025-06-10 16:00:09 +0800

    lazygit tab issue

--- lua/plugins/override/snacks.lua
+++ lua/plugins/override/snacks.lua
@@ -13,6 +13,15 @@ return {
     },
     ---@type snacks.Config
     opts = {
+      lazygit = {
+        config = {
+          os = {
+            edit = '[ -z "$NVIM" ] && (nvim -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{filename}})',
+            editAtLine = '[ -z "$NVIM" ] && (nvim +{{line}} -- {{filename}}) || (nvim --server "$NVIM" --remote-send "q" &&  nvim --server "$NVIM" --remote {{filename}} && nvim --server "$NVIM" --remote-send ":{{line}}<CR>")',
+            openDirInEditor = '[ -z "$NVIM" ] && (nvim -- {{dir}}) || (nvim --server "$NVIM" --remote-send "q" && nvim --server "$NVIM" --remote {{dir}})',
+          },
+        },
+      },
       dashboard = {
         enabled = true,
         preset = {
