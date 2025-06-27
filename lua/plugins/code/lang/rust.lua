9143a0bbb032a21eb0b17b145d26213599c373a8
ogios <ogios@foxmail.com>
2025-06-27 12:12:55 +0800

    rust

--- lua/plugins/code/lang/rust.lua
+++ lua/plugins/code/lang/rust.lua
@@ -1,3 +1,94 @@
+local function postfix_wrap_call(trig, call, requires)
+  return {
+    postfix = trig,
+    body = {
+      call .. "(${receiver})",
+    },
+    requires = requires,
+    scope = "expr",
+  }
+end
+local function postfix_wrap_type(trig, call, requires)
+  return {
+    postfix = trig,
+    body = {
+      call .. "<${receiver}>",
+    },
+    requires = requires,
+    scope = "type",
+  }
+end
+local snippets = {
+  custom = {
+
+    ["Extend::extend"] = postfix_wrap_call("extend", "_.extend"),
+    ["Arc::new"] = postfix_wrap_call("arc", "Arc::new", "std::sync::Arc"),
+    ["Mutex::new"] = postfix_wrap_call("mutex", "Mutex::new", "std::sync::Mutex"),
+    ["RefCell::new"] = postfix_wrap_call("refcell", "RefCell::new", "std::cell::RefCell"),
+    ["Cell::new"] = postfix_wrap_call("cell", "Cell::new", "std::cell::Cell"),
+    ["Rc::new"] = postfix_wrap_call("rc", "Rc::new", "std::rc::Rc"),
+    ["Box::pin"] = postfix_wrap_call("pin", "Box::pin"),
+    ["Some"] = postfix_wrap_call("some", "Some", nil),
+    ["Ok"] = postfix_wrap_call("ok", "Ok", nil),
+    ["Err"] = postfix_wrap_call("err", "Err", nil),
+    ["Option"] = postfix_wrap_type("option_type", "Option", nil),
+    ["RefCell"] = postfix_wrap_type("refcell_type", "RefCell", nil),
+    ["Rc"] = postfix_wrap_type("rc_type", "Rc", nil),
+    ["Box"] = postfix_wrap_type("box_type", "Box", nil),
+    ["unsafe"] = {
+      postfix = "unsafe",
+      body = { "unsafe { ${receiver} }" },
+      description = "Wrap in unsafe{}",
+      scope = "expr",
+    },
+    ["for"] = {
+      postfix = "for",
+      body = { [[for ${1:i} in ${receiver} {
+              $0
+              }]] },
+      description = "Wrap in for _ in _ {}",
+      scope = "expr",
+    },
+    ["if"] = {
+      postfix = "if",
+      body = { [[if ${receiver} {
+              $0
+              }]] },
+      scope = "expr",
+      description = "Wrap in if _ {}",
+    },
+    ["thread::spawn"] = {
+      prefix = "spawn",
+      body = {
+        "thread::spawn(move || {",
+        "\t$0",
+        "});",
+      },
+      description = "Spawn a new thread",
+      requires = "std::thread",
+    },
+    ["channel"] = {
+      prefix = "channel",
+      body = { "let (tx,rx) = mpsc::channel()" },
+      description = "(tx,rx) = channel()",
+      requires = "std::sync::mpsc",
+    },
+    ["from"] = {
+      postfix = "from",
+      body = {
+        "${0:From}::from(${receiver})",
+      },
+      scope = "expr",
+    },
+
+    ["println directly"] = {
+      postfix = "pl",
+      body = 'println!("{}", ${receiver});',
+      scope = "expr",
+    },
+  },
+}
+
 return {
   {
     "mrcjkb/rustaceanvim",
@@ -46,6 +137,10 @@ return {
         vim.keymap.set("n", "K", function()
           vim.cmd.RustLsp({ "hover", "actions" })
         end, { desc = "Hover action (Rust)", buffer = bufnr })
+
+        vim.keymap.set("n", "<C-k>", function()
+          vim.cmd.RustLsp({ "hover", "range" })
+        end, { desc = "Hover range (Rust)", buffer = bufnr })
       end
 
       local setup_feature = function()
@@ -99,19 +194,52 @@ return {
         opts.server.default_settings["rust-analyzer"].cargo = cargo
       end
 
-      -- snippet
       opts.server.default_settings["rust-analyzer"].completion = {
-        snippets = {
-          custom = {
-            ["println directly"] = {
-              postfix = "pl",
-              body = 'println!("{}", ${receiver});',
-              scope = "expr",
-            },
-          },
-        },
+        snippets = snippets,
+        postfix = { enable = true },
+        fullFunctionSignatures = { enable = true },
+        termSearch = { enable = true },
       }
+      -- local st = opts.server.default_settings["rust-analyzer"]
+      -- opts.server.default_settings["rust-analyzer"] = vim.tbl_extend("force", st or {}, {
+      --   diagnostics = {
+      --     enable = true,
+      --     experimental = {
+      --       enable = true,
+      --     },
+      --   },
+      --   completion = {
+      --     snippets = snippets,
+      --     postfix = { enable = true },
+      --     fullFunctionSignatures = { enable = true },
+      --     termSearch = { enable = true },
+      --   },
+      --   hover = {
+      --     actions = {
+      --       enable = true,
+      --       references = { enable = true },
+      --       run = { enable = true },
+      --     },
+      --   },
+      --   rustfmt = {
+      --     rangeFormatting = { enable = true },
+      --   },
+      --   typing = {
+      --     autoClosingAngleBrackets = { enable = true },
+      --   },
+      -- })
 
+      -- snippet
+      -- LazyVim.info(vim.inspect(opts.server.default_settings["rust-analyzer"].completion))
+      -- opts.server.default_settings["rust-analyzer"].completion.snippets = {
+      --   custom = {
+      --     ["println directly"] = {
+      --       postfix = "pl",
+      --       body = 'println!("{}", ${receiver});',
+      --       scope = "expr",
+      --     },
+      --   },
+      -- }
       setup_cfgs()
       setup_no_default_target()
       setup_feature()
