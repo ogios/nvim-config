06cbf488ccc3384dbdfb7af63b3ca03e08d4d817
ogios <ogios@foxmail.com>
2026-06-30 15:25:30 +0800

    remove dep of copilot for sidekick

--- lua/plugins/others/sidekick.lua
+++ lua/plugins/others/sidekick.lua
@@ -10,23 +10,23 @@ return {
       },
     },
   },
-  dependencies = {
-    "zbirenbaum/copilot.lua",
-  },
+  -- dependencies = {
+  --   "zbirenbaum/copilot.lua",
+  -- },
   event = "VeryLazy",
   keys = {
-    {
-      "<Tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require("sidekick").nes_jump_or_apply() then
-          return "<Tab>" -- fallback to normal tab
-        end
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-      mode = { "n", "v" },
-    },
+    -- {
+    --   "<Tab>",
+    --   function()
+    --     -- if there is a next edit, jump to it, otherwise apply it if any
+    --     if not require("sidekick").nes_jump_or_apply() then
+    --       return "<Tab>" -- fallback to normal tab
+    --     end
+    --   end,
+    --   expr = true,
+    --   desc = "Goto/Apply Next Edit Suggestion",
+    --   mode = { "n", "v" },
+    -- },
     {
       "<c-.>",
       function()
@@ -90,12 +90,12 @@ return {
       desc = "Sidekick Select Prompt",
     },
     -- Example of a keybinding to open Claude directly
-    {
-      "<leader>ac",
-      function()
-        require("sidekick.cli").toggle({ name = "claude", focus = true })
-      end,
-      desc = "Sidekick Toggle Claude",
-    },
+    -- {
+    --   "<leader>ac",
+    --   function()
+    --     require("sidekick.cli").toggle({ name = "claude", focus = true })
+    --   end,
+    --   desc = "Sidekick Toggle Claude",
+    -- },
   },
 }
