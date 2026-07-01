1c88071a63459549c3724e0efca1fe1ddedf38a4
ogios <ogios@foxmail.com>
2026-07-01 11:02:45 +0800

    disable copilot

--- lua/plugins/others/sidekick.lua
+++ lua/plugins/others/sidekick.lua
@@ -10,23 +10,23 @@ return {
       },
     },
   },
-  dependencies = {
-    "zbirenbaum/copilot.lua",
-  },
+  -- dependencies = {
+  --   "zbirenbaum/copilot.lua",
+  -- },
   event = "VeryLazy",
   keys = {
-    {
-      "<Tab>",
-      function()
-        -- if there is a next edit, jump to it, otherwise apply it if any
-        if not require("sidekick").nes_jump_or_apply() then
-          return "<Tab>" -- fallback to normal tab
-        end
-      end,
-      expr = true,
-      desc = "Goto/Apply Next Edit Suggestion",
-      mode = { "n", "v" },
-    },
+    -- {
+    --   "<Tab>",
+    --   function()
+    --     -- if there is a next edit, jump to it, otherwise apply it if any
+    --     if not require("sidekick").nes_jump_or_apply() then
+    --       return "<Tab>" -- fallback to normal tab
+    --     end
+    --   end,
+    --   expr = true,
+    --   desc = "Goto/Apply Next Edit Suggestion",
+    --   mode = { "n", "v" },
+    -- },
     {
       "<c-.>",
       function()
