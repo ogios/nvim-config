98e5b569af4d094d5bc435648a4c402e87680664
ogios <ogios@foxmail.com>
2026-07-01 12:46:03 +0800

    codeium

--- lua/plugins/others/neocodeium.lua
+++ lua/plugins/others/neocodeium.lua
@@ -1,10 +1,47 @@
 -- add this to the file where you setup your other plugins:
 return {
-  "monkoose/neocodeium",
+  "ogios/neocodeium",
   event = "VeryLazy",
+  keys = {
+    {
+      "<tab>",
+      function()
+        require("neocodeium").accept()
+      end,
+      mode = "i",
+      desc = "Accept",
+    },
+    -- accept word
+    {
+      "<M-q>",
+      function()
+        require("neocodeium").accept_word()
+      end,
+      mode = "i",
+      desc = "Accept word",
+    },
+    -- use previous suggestion
+    {
+      "<M-a>",
+      function()
+        require("neocodeium").cycle(-1)
+      end,
+      mode = "i",
+      desc = "previous suggestion",
+    },
+    -- use next suggestion
+    {
+      "<M-s>",
+      function()
+        require("neocodeium").cycle(1)
+      end,
+      mode = "i",
+      desc = "next suggestion",
+    },
+  },
+
   config = function()
     local neocodeium = require("neocodeium")
     neocodeium.setup()
-    vim.keymap.set("i", "<C-q>", neocodeium.accept)
   end,
 }
